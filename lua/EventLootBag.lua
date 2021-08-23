a4479996c158eb903f64c175238c6fda52f4c9a4
RexTheWho <jrcrex1@gmail.com>
2021-08-23 14:08:47 +0800

    Fixed lootbag crash on viewer, Added peer colors back to heisters.

--- lua/EventLootBag.lua
+++ lua/EventLootBag.lua
@@ -1,7 +1,7 @@
 
 -- Handles bag tracking.
 
-Hooks:PostHook(PlayerManager, "sync_carry_data", "Heatmap_sync_carry_data", function(self, unit, carry_id, carry_multiplier, dye_initiated, has_dye_pack, dye_value_multiplier, position, dir, throw_distance_multiplier_upgrade_level, zipline_unit, peer_id)
+Hooks:PostHook(PlayerManager, "sync_carry_data", "Heatmap_sync_carry_data", function(self, unit)
 	local event_data = unit
 	HeatMap:UnitAddTracker(event_data)
 end)
